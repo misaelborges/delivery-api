7ed3a76d01238a738e803d5f37d5db4707477a45
Misael Borges Cancelier <mizaelborges2011@gmail.com>
2024-03-09 11:07:07 -0300

    project created

---
+++ src/main/java/com/misaeborges/deliveryapi/DeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.misaeborges.deliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryApiApplication.class, args);
+	}
+
+}
