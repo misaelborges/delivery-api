8c124cc71b17bd94c61ef68abb4df9f6a235bcc2
Misael Borges Cancelier <mizaelborges2011@gmail.com>
2024-03-14 16:17:13 -0300

    creating method findAll in CuisineController

--- src/main/java/com/misaeborges/deliveryapi/api/CuisineController.java
+++ src/main/java/com/misaeborges/deliveryapi/api/CuisineController.java
@@ -1,11 +1,15 @@
 package com.misaeborges.deliveryapi.api;
 
+import com.misaeborges.deliveryapi.domain.dto.CuisineResponseDTO;
 import com.misaeborges.deliveryapi.domain.dto.CuisineResquestDTO;
 import com.misaeborges.deliveryapi.domain.model.Cuisine;
+import com.misaeborges.deliveryapi.domain.repositories.ICuisineRespository;
 import com.misaeborges.deliveryapi.domain.services.CuisineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cuisinis")
@@ -13,6 +17,14 @@ public class CuisineController {
 
     @Autowired
     private CuisineService cuisineService;
+
+    @Autowired
+    private ICuisineRespository cuisineRespository;
+
+    @GetMapping
+    public List<CuisineResponseDTO> findAll() {
+        return cuisineRespository.findAll().stream().map(CuisineResponseDTO::new).toList();
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
