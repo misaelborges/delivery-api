7ec5ac6ed764bb73a37448f04e31f740bcfc9ae6
Misael Borges Cancelier <mizaelborges2011@gmail.com>
2024-04-03 16:01:34 -0300

    creating method update in CuisineController

--- src/main/java/com/misaeborges/deliveryapi/api/CuisineController.java
+++ src/main/java/com/misaeborges/deliveryapi/api/CuisineController.java
@@ -5,6 +5,7 @@ import com.misaeborges.deliveryapi.domain.dto.CuisineResquestDTO;
 import com.misaeborges.deliveryapi.domain.model.Cuisine;
 import com.misaeborges.deliveryapi.domain.repositories.ICuisineRespository;
 import com.misaeborges.deliveryapi.domain.services.CuisineService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -35,5 +36,13 @@ public class CuisineController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cuisine save(@RequestBody CuisineResquestDTO cuisineResquestDTO) {
         return cuisineService.save(cuisineResquestDTO);
+    }
+
+    @PutMapping("/{id}")
+    public Cuisine upload(@RequestBody CuisineResquestDTO cuisineData, @PathVariable Long id) {
+        Cuisine cuisine = cuisineService.searchEngine(id);
+
+        BeanUtils.copyProperties(cuisineData, cuisine, "id");
+        return cuisineRespository.save(cuisine);
     }
 }
