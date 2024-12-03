41457597fe3a60439b7880c610729952f0f3bb16
Misael Borges Cancelier <mizaelborges2011@gmail.com>
2024-12-03 15:12:52 -0300

    feat(domain): Create Product domain entity

---
+++ src/main/java/com/misaeborges/deliveryapi/domain/models/Product.java
@@ -0,0 +1,37 @@
+package com.misaeborges.deliveryapi.domain.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    private BigDecimal price;
+
+    private Boolean active;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurant restaurant;
+}
